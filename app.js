1ecdd3133f5dee9cf8868b1742220c2cad44327b
NotAName <alexis@notaname.io>
2021-01-01 08:43:26 +0100

    Réalisation de la classe Command et mise en place du dossier Native

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // Soon
+const config = require("./Config/Global.json");
+const discord = require("discord.js");
+
+const client = new discord.Client;
+
+const Command = require("./Custom/Command.js");
+
+let test = new Command("test", "test", "test", ["704533945290588211"])
+
+client.on('message', async message => {
+
+    test.execute(message, function(a, c, mc) {
+        message.reply("coucou");
+    })
+
+});
+
+client.login(config.token);
