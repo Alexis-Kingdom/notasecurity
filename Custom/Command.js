f7c918f782bf75ebe3575c6b082fb163bab16852
NotAName <alexis@notaname.io>
2021-01-03 13:02:10 +0100

    Déploiement de la maj

--- Custom/Command.js
+++ Custom/Command.js
@@ -1,14 +1,71 @@
 class Command {
 
-    constructor(title, description, name, restriction) {
-        this.title = title
-        this.description = description
-        this.name = name
-        this.restriction = restriction
-        this.prefix = "nas!"
+    constructor(options) {
+
+        // 
+        // Translator
+        // 
+        this.TranslateUtil = require("../Utils/Translate.js")
+
+        // 
+        // File config of the base
+        // 
+        this.Base = require("../Text/base.json")
+
+        // 
+        // File config of the command
+        // 
+        this.Text = require("../Text/"+ options.name +".json")
+
+        // 
+        // Error manager
+        // 
         this.Error = require("../Native/Error.js")
+
+        // 
+        // Log manager
+        // 
+        this.LogUtil = require("../Utils/Log.js")
+
+        // 
+        // Name of the command
+        // 
+        this.name = options.name
+
+        // 
+        // List of the restriction
+        // 
+        this.restriction = options.restriction
+
+        // 
+        // Log status (true or false)
+        // 
+        this.logStatus = options.log
+
+        // 
+        // Args (null or number)
+        // 
+        this.args = options.args
+
+        // 
+        // The prefix of all commands
+        // 
+        this.prefix = "nas!"
+
+        // 
+        // Instance of translator
+        // 
+        this.translate = new this.TranslateUtil
+
+        // 
+        // Async function init
+        // 
+        this.init()
     }
 
+    // 
+    // Restriction Control
+    // 
     restrictionControl(message) {
         if(this.restriction == null) return true;
         let control = this.restriction.some((role) => {
@@ -17,16 +74,49 @@ class Command {
         return control;
     }
 
-    execute(message, callback) {
+    // 
+    // Execute
+    // 
+    async execute(message, callback) {
 
-        if(!message.content.startsWith(this.prefix)) return;
+        let cmdLogRestrict = await this.translate.translate(this.Base.cmdLogRestrict, this.Base.lang)
+        let cmdRestrictMessage = await this.translate.translate(this.Base.cmdRestrictMessage, this.Base.lang)
+        let cmdLogUse = await this.translate.translate(this.Base.cmdLogUse, this.Base.lang)
 
-        if(message.content !== this.prefix + this.name) return;
+        if (!message.content.startsWith(this.prefix) || message.author.bot) return;
 
-        if(this.restrictionControl(message) == false) return new this.Error(message, "Vous ne pouvez pas utiliser cette commande.");
+        let args = message.content.slice(this.prefix.length).trim().split(' ');
+        const command = args.shift().toLowerCase();
 
-        callback(message.author, message.content, message.channel);
+        if(command !== this.name) return;
 
+        if(this.args !== null) args = message.content.split(' ').splice(1).join(' ')
+
+        if(this.restrictionControl(message) == false) {
+            if(this.logStatus == true) this.log(message, cmdLogRestrict + this.prefix + this.name, "utilisation");
+            return new this.Error(message, cmdRestrictMessage);
+        } else if(this.logStatus == true) {
+            this.log(message, cmdLogUse + this.prefix + this.name, "utilisation")
+        }
+
+        callback(this.Text, this.translate, args);
+
+    }
+
+    // 
+    // Write log
+    // 
+    log(message, description, action) {
+        let log = new this.LogUtil(this.name)
+        log.write(message, description, action)
+    }
+
+    // 
+    // Init
+    // 
+    async init() {
+        this.title = await this.translate.translate(this.Text.title, this.Text.lang)
+        this.description = await this.translate.translate(this.Text.description, this.Text.lang)
     }
 
 }
